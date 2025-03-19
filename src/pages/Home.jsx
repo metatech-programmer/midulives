4e9199dac6a16b52a275afc795fb4e0f62fb2d59
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2025-03-19 15:41:37 -0500

    fix(Home): update background image path to use absolute URL

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
 
   return (
     <div className="h-[80dvh] w-full flex flex-col items-center justify-center text-white relative animate-fade-in">
-      <div className="fixed inset-0 bg-noise bg-[url(./giphy.gif)] bg-cover opacity-10  bg-center [mask-image:radial-gradient(circle,#000_10%,transparent_80%)]" />
+      <div className="fixed inset-0 bg-noise bg-[url(/giphy.gif)] bg-cover opacity-10  bg-center [mask-image:radial-gradient(circle,#000_10%,transparent_80%)]" />
       <div className="z-10 text-center">
         <h1 className="text-8xl font-extrabold my-6  text-purple-700 font-silkscreen [text-shadow:0_0_10px_purple]">
           {t("home.title")}
